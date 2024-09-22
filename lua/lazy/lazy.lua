32fc67ca33e154b5a957e806c9c60ad39424a5f4
Coen Honingh <coenhoningh@gmail.com>
2024-09-22 02:33:18 +0200

    nog minder

--- lua/lazy/lazy.lua
+++ lua/lazy/lazy.lua
@@ -7,3 +7,47 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   end
 end
 vim.opt.rtp:prepend(lazypath)
+require('lazy').setup({
+  require 'lsp.lsp',
+  require 'config.theme',
+  {
+    import = 'plugins',
+  },
+}, {
+  ui = {
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  },
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  performance = {
+    cache = { enabled = true },
+    rtp = {
+      disabled_plugins = {
+        'gzip',
+        'matchit',
+        'matchparen',
+        'netrwPlugin',
+        'tarPlugin',
+        'tohtml',
+        'tutor',
+        'zipPlugin',
+      },
+    },
+  },
+})
